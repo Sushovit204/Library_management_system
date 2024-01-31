eba95a23a7773281f1a63e29a74e2b6f48bb7abc
Sushovit204 <shahsushovit434@gmail.com>
2024-01-31 23:45:00 +0545

    Completed the book API now borrowed API remains

--- app/api/routers/book.py
+++ app/api/routers/book.py
@@ -1,4 +1,5 @@
 from schemas.book import Books
+from schemas.book_detail import BookDetails
 from psycopg2 import DatabaseError, OperationalError, ProgrammingError
 from fastapi import HTTPException, status, APIRouter
 from db.database import create_connection, create_cursor
@@ -98,6 +99,49 @@ def get_user_by_id(book_id: int):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Internal Server Error",
         ) from e
+    finally:
+        mycursor.close()
+        mydb.close()
+
+# Assign/Update Book Details: Endpoint to assign details to a book or update existing book details
+@router.put("/update/{book_id}", status_code=status.HTTP_200_OK)
+def assign_update_book_details(book_id: int, book_details: BookDetails):
+    try:
+        # Create a connection
+        mydb = create_connection()
+
+        # Create a cursor
+        mycursor = create_cursor(mydb)
+
+        # Check if a row with the specified book_id exists in book_details
+        query_check = "SELECT details_id FROM BOOK_DETAILS WHERE book_id=%s"
+        val_check = (book_id,)
+        mycursor.execute(query_check, val_check)
+        existing_details_id = mycursor.fetchone()
+
+        if existing_details_id:
+            # If a row exists, update it
+            query_update = "UPDATE BOOK_DETAILS SET number_of_pages=%s, publisher=%s, language=%s WHERE book_id=%s"
+            val_update = (book_details.number_of_pages, book_details.publisher, book_details.language, book_id)
+            mycursor.execute(query_update, val_update)
+        else:
+            # If no row exists, insert a new row
+            query_insert = "INSERT INTO BOOK_DETAILS(book_id, number_of_pages, publisher, language) VALUES (%s, %s, %s, %s)"
+            val_insert = (book_id, book_details.number_of_pages, book_details.publisher, book_details.language)
+            mycursor.execute(query_insert, val_insert)
+
+        # Commit the transaction
+        mydb.commit()
+
+        return {"data": f"Book details updated successfully for BookID {book_id}"}
+
+    except (ProgrammingError, OperationalError, DatabaseError) as e:
+        print(f"Database Error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal Server Error",
+        ) from e
+
     finally:
         mycursor.close()
         mydb.close()
--- app/schemas/book_detail.py
+++ app/schemas/book_detail.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 
 class BookDetails(BaseModel):
-    book_id : int
     number_of_pages : int
     publisher : str
     language : str 
